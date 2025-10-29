120bb6f6c2cbcf23c7c8e6db10b51c68d2cb7de1
Kenneth Obsequio <k80308392@gmail.com>
2025-10-29 20:44:10 +0800

    migrate: from AWS to Azure (#2)
    I hope everything looks fine after all
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -9,12 +9,12 @@
 * Change History:
 * DATE                AUTHOR            LOG NUMBER     DESCRIPTION                                                      
 * Oct. 6, 2025        rgrgogu           001            Initial creation - STAR Phase 1 Project
+* Oct 29, 2025        lash0000          002            Initial creation - Migrate to Azure Functions
 ***********************************************************************************************************************************************************************/
 
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const serverless = require('serverless-http');
 const cookieParser = require('cookie-parser');
 const routes_v1 = require('./src/utils/routes_v1.utils');
 const sequelize = require('./config/db.config');
@@ -22,10 +22,19 @@ const sequelize = require('./config/db.config');
 dotenv.config();
 const app = express();
 
+app.set('trust proxy', 1);
+
 // JSON Payload
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  })
+);
 app.use(cookieParser());
 
 // Load all
@@ -35,9 +44,9 @@ app.use('/api/v1/data', routes_v1);
 app.get('/', (req, res) => {
   res.json({
     project_name: "Sales Training and Recruitment System by Philproperties",
-    project_overview: "It is a serverless architecture made via REST API design with Sequelize, Nodemailer, Elasticache and S3",
+    project_overview: "It is a stateless architecture with REST principle including Sequelize, Nodemailer and Azure Functions",
     version: "0xx",
-    description: "A REST API method for STAR product.",
+    description: "A REST API method for STAR online courses product.",
     available_routes: [
       "/api/v1/data/xxx"
     ]
@@ -55,12 +64,8 @@ const db_postgres_connection = async () => {
 }
 db_postgres_connection();
 
-// Export handler for Serverless
-module.exports.handler = serverless(app);
+// Start server (Azure & local friendly)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-if (require.main === module) {
-  const CFG_PORT = process.env.PORT || 3000;
-  app.listen(CFG_PORT, () => {
-    console.log(`Server running on port ${CFG_PORT}`);
-  });
-}
+module.exports = app;
